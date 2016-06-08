f95f84194691a65179dafbe2945e999e229949ce
NISH1001 <nishanpantha@gmail.com>
2016-06-08 18:14:04 +0545

    Scrap locations and accomodations

--- zillow_scraper/zillow_scraper/spiders/zillow_manual_spider.py
+++ zillow_scraper/zillow_scraper/spiders/zillow_manual_spider.py
@@ -4,6 +4,8 @@ from scrapy.http import Request
 
 from zillow_scraper.items import ZillowScraperItem
 
+import re
+
 class TestSpider(BaseSpider):
     name = "zillowspider"
     allowed_domains = ["zillow.com"]
@@ -17,11 +19,11 @@ class TestSpider(BaseSpider):
 
         # extract the links for detail page
         links = hxs.xpath("//a[contains(@class,'hdp-link routable')]/@href").extract()
-        for index, link in enumerate(links):
+        for link in links:
             full_url = "http://zillow.com" + link
             
             # create new request for details page
-            yield Request(full_url, callback=self.parse_home, meta={'count' : index})
+            yield Request(full_url, callback=self.parse_home, meta={'url' : full_url})
 
             url = response.urljoin(response.xpath("//a[contains(.//text(), \'Next\')]/@href").extract_first())
             # create new request for next page
@@ -31,6 +33,30 @@ class TestSpider(BaseSpider):
     def parse_home(self, response):
         hxs = HtmlXPathSelector(response)
         item = ZillowScraperItem()
-        item['location'] = response.xpath("//header[@class='zsg-content-header addr']/h1/text()").extract_first().strip()[:-1]
+
+        item['url'] = response.meta['url']
+
+        # extract locations 
+        city_state_code = response.xpath("//header[@class='zsg-content-header addr']/h1/span/text()").extract_first().split(",")
+        state_code = city_state_code[1].strip().split(" ")
+        item['address'] = response.xpath("//header[@class='zsg-content-header addr']/h1/text()").extract_first().strip()[:-1]
+        item['city'] = city_state_code[0].strip()
+        item['state'] = state_code[0].strip()
+        item['zip_code'] = state_code[1].strip()
+
+        # extract accomodations
+        accomodations = response.xpath("//header[@class='zsg-content-header addr']/h3/span[@class='addr_bbs']/text()").extract()
+        item['beds'] = ''.join(re.findall(r'\d+', accomodations[0]))
+        item['baths'] = ''.join(re.findall(r'\d+', accomodations[1]))
+        item['sq_ft'] = accomodations[2]
+
+        # extract price
+        price = response.xpath("//div[@class='main-row home-summary-row']/span/text()")
+        print(price)
+
         yield item
+
+
+
+
 
