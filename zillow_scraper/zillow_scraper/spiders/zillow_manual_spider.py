e15c3124a2a7b926f0f4af37a3f2545067e64a54
NISH1001 <nishanpantha@gmail.com>
2016-06-09 11:14:43 +0545

    Scrap basic datapoints

--- zillow_scraper/zillow_scraper/spiders/zillow_manual_spider.py
+++ zillow_scraper/zillow_scraper/spiders/zillow_manual_spider.py
@@ -6,6 +6,10 @@ from zillow_scraper.items import ZillowScraperItem
 
 import re
 
+def lowercase(string):
+    string = string.lower().strip()
+    return re.sub(r'\s+', '_', string)
+
 class TestSpider(BaseSpider):
     name = "zillowspider"
     allowed_domains = ["zillow.com"]
@@ -48,11 +52,30 @@ class TestSpider(BaseSpider):
         accomodations = response.xpath("//header[@class='zsg-content-header addr']/h3/span[@class='addr_bbs']/text()").extract()
         item['beds'] = ''.join(re.findall(r'\d+', accomodations[0]))
         item['baths'] = ''.join(re.findall(r'\d+', accomodations[1]))
-        item['sq_ft'] = accomodations[2]
+        item['sq_ft'] = ''.join(re.findall(r'\d+', accomodations[2]))
 
         # extract price
-        price = response.xpath("//div[@class='main-row home-summary-row']/span/text()")
-        print(price)
+        item['price'] = response.xpath("//div[contains(@class, 'main-row')]/span/text()").extract_first().strip()
+
+        # extract the facts, features, etc using the list of selectors
+        selectors = response.xpath("//div[contains(@class,'fact-group-container')]")
+        for sel in selectors:
+            # get header title
+            title = sel.xpath("./h3/text()").extract_first()
+            try:
+                item[lowercase(title)] = [ fact for fact in sel.xpath(".//li/text()").extract() ]
+            except KeyError:
+                continue
+
+        # extract county website
+        cw = response.xpath("//a[contains(.//text(), \'County website\')]/@href").extract_first() 
+        item['county_website'] = cw if cw is not None else ''
+
+        # extract agent names
+        agent_divs = response.xpath("//div[contains(@class, 'recipients')]")
+        names = agent_divs[0].xpath(".//a[@class='profile-name-link']/text()").extract()
+        phones = agent_divs[0].xpath(".//span[@class='snl phone']/text()").extract()
+        item['agents'] = [ {'name' : name, 'phone' : phones[index]} for index, name in enumerate(names) ] if names and phones else []
 
         yield item
 
