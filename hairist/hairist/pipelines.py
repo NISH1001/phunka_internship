84b6e8e3ddc313ac846fd9ea84a6da1b614d12f4
NISH1001 <nishanpantha@gmail.com>
2016-08-02 20:14:47 +0545

    Handle duplication insertion

--- hairist/hairist/pipelines.py
+++ hairist/hairist/pipelines.py
@@ -10,6 +10,7 @@ from scrapy.http import Request
 
 from sqlalchemy import inspect
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 
 from hairist import settings
 from hairist.models import Hairist, db_connect, DeclarativeBase
@@ -40,24 +41,29 @@ class PostgresPipeline:
         DeclarativeBase.metadata.create_all(engine)
 
     def insert(self, item):
-        engine = db_connect()
-        Session = sessionmaker(bind=engine)
-        session = Session()
+        try:
+            engine = db_connect()
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-        mapper = inspect(Hairist)
-        attr_names = [c_attr.key for c_attr in mapper.mapper.column_attrs]
+            mapper = inspect(Hairist)
+            attr_names = [c_attr.key for c_attr in mapper.mapper.column_attrs]
 
-        d = {}
-        for attr in attr_names:
-            try:
-                d[attr] = str(item[attr])
-                #print(type(d[attr]), attr,  d[attr])
-            except KeyError:
-                d[attr] = ''
-        amp = Hairist(**d)
-        session.add(amp)
-        session.commit()
-        session.close()
+            d = {}
+            for attr in attr_names:
+                try:
+                    d[attr] = str(item[attr])
+                    #print(type(d[attr]), attr,  d[attr])
+                except KeyError:
+                    d[attr] = ''
+            amp = Hairist(**d)
+            session.add(amp)
+            session.commit()
+            session.close()
+        except IntegrityError:
+            print("="*50)
+            print("already exists... cannot insert")
+            return
 
 
 class HairistImagePipeline(ImagesPipeline):
@@ -69,35 +75,38 @@ class HairistImagePipeline(ImagesPipeline):
     # stuffs to do after the request is completed
     def item_completed(self, results, item, info):
         for result in [x for ok, x in results if ok]:
-            print("="*30)
-            # original path
-            path = result['path']
+            try:
+                print("="*30)
+                # original path
+                path = result['path']
 
-            # get full storage path
-            storage = settings.IMAGES_STORE
+                # get full storage path
+                storage = settings.IMAGES_STORE
 
-            # new folder name according to id
-            my_path = str(item['id'])
+                # new folder name according to id
+                my_path = str(item['id'])
 
 
-            # new path according to id
-            new_path = os.path.join(storage, my_path)
+                # new path according to id
+                new_path = os.path.join(storage, my_path)
 
-            # filename
-            filename = os.path.basename(path)
+                # filename
+                filename = os.path.basename(path)
 
-            # path to original image
-            path = os.path.join(storage, path)
+                # path to original image
+                path = os.path.join(storage, path)
 
-            # new path
-            target_path = os.path.join(storage, my_path)
+                # new path
+                target_path = os.path.join(storage, my_path)
 
-            item['image_local_path'] = target_path
+                item['image_local_path'] = target_path
 
-            # now move to the new path
-            if not os.path.exists(target_path):
-                os.makedirs(target_path)
-            shutil.move(path, target_path)
+                # now move to the new path
+                if not os.path.exists(target_path):
+                    os.makedirs(target_path)
+                shutil.move(path, target_path)
+            except shutil.Error:
+                continue
         return item
 
 
